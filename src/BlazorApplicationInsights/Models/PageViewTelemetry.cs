0934104c1421bb5eda882d80b70085986673d0a5
Glen Banta <glen@corracing.com>
2024-04-03 17:43:18 -0700

    kjuikol

--- src/BlazorApplicationInsights/Models/PageViewTelemetry.cs
+++ src/BlazorApplicationInsights/Models/PageViewTelemetry.cs
@@ -1,45 +1,45 @@
 ﻿namespace BlazorApplicationInsights.Models;
-using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 /// <summary>
 /// Pageview telemetry class, extending IPartC.
 /// </summary>
-public class PageViewTelemetry : PartC
-{
-    /// <summary>
-    /// The string you used as the name in startTrackPage. Defaults to the document title.
-    /// </summary>
-    [JsonPropertyName("name")]
-    public string? Name { get; set; }
+public class PageViewTelemetry : PartC {
+	/// <summary>
+	/// The string you used as the name in startTrackPage. Defaults to the document title.
+	/// </summary>
+	[JsonPropertyName("name")]
+	public string? Name { get; set; }
 
-    /// <summary>
-    /// A relative or absolute URL that identifies the page or other item. Defaults to the window location.
-    /// </summary>
-    [JsonPropertyName("uri")]
-    public string? Uri { get; set; }
+	/// <summary>
+	/// A relative or absolute URL that identifies the page or other item. Defaults to the window location.
+	/// </summary>
+	[JsonPropertyName("uri")]
+	public string? Uri { get; set; }
 
-    /// <summary>
-    /// The URL of the source page where the current page is loaded from.
-    /// </summary>
-    [JsonPropertyName("refUri")]
-    public string? RefUri { get; set; }
+	/// <summary>
+	/// The URL of the source page where the current page is loaded from.
+	/// </summary>
+	[JsonPropertyName("refUri")]
+	public string? RefUri { get; set; }
 
-    /// <summary>
-    /// Page type.
-    /// </summary>
-    [JsonPropertyName("pageType")]
-    public string? PageType { get; set; }
+	/// <summary>
+	/// Page type.
+	/// </summary>
+	[JsonPropertyName("pageType")]
+	public string? PageType { get; set; }
 
-    /// <summary>
-    /// Boolean indicating whether the user is logged in.
-    /// </summary>
-    [JsonPropertyName("isLoggedIn")]
-    public bool? IsLoggedIn { get; set; }
+	/// <summary>
+	/// Boolean indicating whether the user is logged in.
+	/// </summary>
+	[JsonPropertyName("isLoggedIn")]
+	public bool? IsLoggedIn { get; set; }
 
-    /// <summary>
-    /// Custom defined iKey.
-    /// </summary>
-    [JsonPropertyName("iKey")]
-    public string? iKey { get; set; }
+	/// <summary>
+	/// Custom defined iKey.
+	/// </summary>
+	[JsonPropertyName("iKey")]
+#pragma warning disable IDE1006 // Naming Styles
+	public string? iKey { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
 }
